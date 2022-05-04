b46518fae567b0222cb4cb9bbba9c6f1a3cd631c
Akshaya <rakshayaa97@gmail.com>
2022-05-04 13:30:14 +0530

    Sutrix Assessment

---
+++ src/data.js
@@ -0,0 +1,93 @@
+export const personData = {
+    firstname: 'Juan',
+    lastname: 'Dela Cruz',
+    birthday: '01/01/2000',
+}
+
+export const productData =
+    [
+        {
+            "brand": "Toyota",
+            "model": "Wigo",
+            "bodyStyle": "Hatchback",
+            "variant": "TRD",
+            "price": "700,000"
+        },
+        {
+            "brand": "Toyota",
+            "model": "Wigo",
+            "bodyStyle": "Hatchback",
+            "variant": "G",
+            "price": "658,000"
+        },
+        {
+            "brand": "Toyota",
+            "model": "Yaris",
+            "bodyStyle": "Hatchback",
+            "variant": "S",
+            "price": "1,114,000"
+        },
+        {
+            "brand": "Toyota",
+            "model": "Yaris",
+            "bodyStyle": "Hatchback",
+            "variant": "E",
+            "price": "973,000"
+        },
+        {
+            "brand": "Toyota",
+            "model": "Vios",
+            "bodyStyle": "Sedan",
+            "variant": "G",
+            "price": "970,000"
+        },
+        {
+            "brand": "Toyota",
+            "model": "Vios",
+            "bodyStyle": "Sedan",
+            "variant": "XLE",
+            "price": "851,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "Brio",
+            "bodyStyle": "Hatchback",
+            "variant": "RS",
+            "price": "735,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "Brio",
+            "bodyStyle": "Hatchback",
+            "variant": "V",
+            "price": "661,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "City",
+            "bodyStyle": "Hatchback",
+            "variant": "RS",
+            "price": "1,115,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "City",
+            "bodyStyle": "Sedan",
+            "variant": "V",
+            "price": "978,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "Civic",
+            "bodyStyle": "Sedan",
+            "variant": "RS",
+            "price": "1,615,000"
+        },
+        {
+            "brand": "Honda",
+            "model": "Civic",
+            "bodyStyle": "Sedan",
+            "variant": "E",
+            "price": "1,188,000"
+        },
+    ]
